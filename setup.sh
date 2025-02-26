dd98438c7f78c0d20ff259b443eeb31bf445a07e
Muhamed Adhil K A <mohd.adhilgafoor@gmail.com>
2025-02-26 22:39:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+sudo apt update -y
+sudo apt install subfinder -y
+sudo apt install assetfinder -y
+sudo apt install findomain -y
+sudo apt install httprobe -y
+
 sudo cp pikachu.sh pikachu
 sudo cp pikachu /bin
